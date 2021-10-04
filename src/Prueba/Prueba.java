efaec29550930aa5b581844eaf9e40e13511678c
David Perez Aviles <david.perez.external@atos.net>
2021-10-04 18:48:39 +0200

    Prueba

--- src/Prueba/Prueba.java
+++ src/Prueba/Prueba.java
@@ -4,8 +4,6 @@ public class HolaMundo {
 
 	public static void main(String args[])
     {
-        System.out.println("Hola Mundo");
-        System.out.println("Prueba");
+        System.out.println("Prueba Rama 2");
     }
 }
-
